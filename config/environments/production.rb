28cb86fdea76ab922473ac5da1a70df33a8acc98
Chris Oliver <excid3@gmail.com>
2017-04-17 18:09:39 -0500

    Better flag for disabling protection

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,5 +84,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_cable.allowed_request_origins = [/*/]
+  config.action_cable.disable_request_forgery_protection = true
 end
